7c34ba58e67d2646f2b4601e0348d7b7d19ff910
suhasbhatcr <suhasbhatcr@gmail.com>
2020-08-08 22:08:48 +0530

    Completed module

--- src/app.js
+++ src/app.js
@@ -4,26 +4,36 @@ const path = require('path');
 const express = require('express');
 const app = new express();
 
-const accountData = fs.readFileSync("src/json/accounts.json");
-const accounts = JSON.parse(accountData).accounts;
-console.log(accounts);
+const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
+const accounts = JSON.parse(accountData);
 
-const userData = fs.readFileSync("src/json/users.json");
-const users = JSON.parse(userData).users;
-console.log(users);
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
+const users = JSON.parse(userData);
 
 app.set('views',path.join(__dirname, '/views'));
 app.set('view engine','ejs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('/', (req, res) => res.render('index', 
-{   title: 'Account Summary',
+app.get('/', (req, res) => res.render('index',{   
+    title: 'Account Summary',
     accounts: accounts
 }));
 
 app.get('/savings',(req, res)=> res.render('account',{
     account: accounts.savings
+}));
+
+app.get('/checking',(req, res)=> res.render('account',{
+    account: accounts.checking
+}));
+
+app.get('/credit',(req, res)=> res.render('account',{
+    account: accounts.credit
+}));
+
+app.get('/profile',(req, res)=> res.render('profile',{
+    user: users[0]
 }));
 
 app.listen(3000, () => { console.log('PS Project Running on port 3000!') });
