a307b16fb7a8ae34c2bc3203b2707bf16632331d
suhasbhatcr <suhasbhatcr@gmail.com>
2020-08-08 21:33:57 +0530

    Completed module

--- src/app.js
+++ src/app.js
@@ -4,11 +4,26 @@ const path = require('path');
 const express = require('express');
 const app = new express();
 
+const accountData = fs.readFileSync("src/json/accounts.json");
+const accounts = JSON.parse(accountData).accounts;
+console.log(accounts);
+
+const userData = fs.readFileSync("src/json/users.json");
+const users = JSON.parse(userData).users;
+console.log(users);
+
 app.set('views',path.join(__dirname, '/views'));
 app.set('view engine','ejs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('/', (req, res) => res.render('index', { title: 'Index' }));
+app.get('/', (req, res) => res.render('index', 
+{   title: 'Account Summary',
+    accounts: accounts
+}));
+
+app.get('/savings',(req, res)=> res.render('account',{
+    account: accounts.savings
+}));
 
 app.listen(3000, () => { console.log('PS Project Running on port 3000!') });
