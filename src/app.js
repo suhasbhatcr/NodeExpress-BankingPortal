fb4277e7ba27e6f2b5064e17c896e25d1570d86a
suhasbhatcr <suhasbhatcr@gmail.com>
2020-08-09 13:28:16 +0530

    Completed module

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ app.set('views',path.join(__dirname, '/views'));
 app.set('view engine','ejs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded);
 
 app.get('/', (req, res) => res.render('index',{   
     title: 'Account Summary',
@@ -34,6 +35,10 @@ app.get('/credit',(req, res)=> res.render('account',{
 
 app.get('/profile',(req, res)=> res.render('profile',{
     user: users[0]
+}));
+
+app.get('/transfer',(req, res)=> res.render('transfer',{
+    
 }));
 
 app.listen(3000, () => { console.log('PS Project Running on port 3000!') });
